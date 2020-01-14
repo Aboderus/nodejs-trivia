054539ec63999e26e3a22bc771da35981ed4288f
Adam Bodérus <adam@boderus.eu>
2020-01-14 22:05:45 +0100

    Test incrementAndFetchQuestion and takeTurn

--- src/game.spec.js
+++ src/game.spec.js
@@ -11,6 +11,30 @@ describe("The test environment", function () {
   });
 });
 
-describe("Your specs...", function () {
-  // it ...
+
+describe("Increment questions", function () {
+  const game = new Game();
+
+  it("should return incrementing questions from category", function () {
+    const categoryOne = game.categories[0];
+    const categoryTwo = game.categories[1]
+    const questionOne = game.incrementAndFetchQuestion(categoryOne);
+    const questionTwo = game.incrementAndFetchQuestion(categoryOne);
+    const questionOtherCategory = game.incrementAndFetchQuestion(categoryTwo);
+    Should(questionOne).equal(`${categoryOne} Question 0`);
+    Should(questionTwo).not.equal(questionOne);
+    Should(questionOne).not.equal(questionOtherCategory);
+  });
+});
+
+describe("takeTurn increments and resets places", function () {
+  const game = new Game();
+
+  it("should increment after each takeTurn. When it goes over 11 it should reset to within span.", function () {
+    game.addPlayer("Alfa");
+    game.takeTurn(10);
+    Should(game.places[game.currentPlayer]).equal(10);
+    game.takeTurn(3);
+    Should(game.places[game.currentPlayer]).equal(1);
+  });
 });
